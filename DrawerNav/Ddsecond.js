b2de95507f97fe24e2fad43791e409e8ac00da64
mostafa <mostafamohsen1999@gmail.com>
2023-04-08 16:04:19 +0200

    DdSecond

---
+++ DrawerNav/Ddsecond.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const DdSecond = () => {
+  return (
+    <View>
+      <Text>DarkMode</Text>
+    </View>
+  )
+}
+
+export default DdSecond
+
+const styles = StyleSheet.create({})
